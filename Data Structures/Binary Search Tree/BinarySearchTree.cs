5e8623884aa58a4304c09ab2ed5403c62e385df8
Viju Rajan <VijuRajan001@github.com>
2021-10-17 18:18:57 +0530

    Adding changes for Inorder predecessor and successor

--- Data Structures/Binary Search Tree/BinarySearchTree.cs
+++ Data Structures/Binary Search Tree/BinarySearchTree.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace Binary_Search_Tree
 {
-    internal class Node<T>
+    public class Node<T>
     {
         public T Item;
         public Node<T> Left { get; set; }
@@ -106,17 +106,228 @@ namespace Binary_Search_Tree
         }
         public virtual bool Add(T item)
         {
-            return Add(item,root);
+            return Add(item, root);
         }
 
+        /*
+            Remove All nodes of a binary tree
+            Algorithms :
+            Post Order Traversal 
+                - Removes leaf nodes first followed by root nodes
+        
+        */
         public void Clear()
         {
             throw new NotImplementedException();
         }
 
-        public bool Contains(T item)
+        public void FindAll()
         {
-            throw new NotImplementedException();
+
+        }
+
+        public void FindAllInRange()
+        {
+
+        }
+
+        internal Node<T> FindMinimum(Node<T> node)
+        {
+            while (node is not null)
+            {
+                node = node.Left;
+            }
+
+            return node;
+
+        }
+
+        internal Node<T> FindMaximum(Node<T> node)
+        {
+            while (node is not null)
+            {
+                node = node.Right;
+            }
+
+            return node;
+
+        }
+        internal Node<T> FindInOrderSuccessor(Node<T> node, T item)
+        {
+
+            if (node == null)
+                return null;
+
+            var order = comparer.Compare(item, node.Item);
+            Node<T> successor = null;
+            if (order == 0)
+            {
+                if (node.Right != null)
+                    return FindMinimum(node.Right);
+            }
+            else if (order < 0)
+            {
+                successor = node;
+                FindInOrderSuccessor(node.Left, item);
+            }
+            else if (order > 0)
+                FindInOrderSuccessor(node.Right, item);
+
+
+            return successor is not null ? successor : null;
+
+        }
+
+        public Node<T> InOrderSuccessor(T item)
+        {
+            return FindInOrderSuccessor(root, item);
+        }
+
+
+        internal Node<T> FindInOrderPredecessor(Node<T> node, T item)
+        {
+
+            if (node == null)
+                return null;
+
+            var order = comparer.Compare(item, node.Item);
+            Node<T> predecessor = null;
+
+            if (order == 0)
+            {
+                if (node.Left != null)
+                    return FindMaximum(node.Left);
+            }
+            else if (order < 0)
+            {
+
+                FindInOrderSuccessor(node.Left, item);
+            }
+            else if (order > 0)
+            {
+                predecessor = node;
+                FindInOrderSuccessor(node.Right, item);
+            }
+
+            return predecessor is not null ? predecessor : null;
+
+        }
+        public Node<T> InOrderPredecessor(T item)
+        {
+            return FindInOrderPredecessor(root,item);
+        }
+
+        internal virtual bool InOrder(Predicate<Node<T>> action, Node<T> node, bool reverse = false)
+        {
+            if (root == null)
+                return false;
+
+            InOrder(action, !reverse ? node.Left : node.Right, reverse);
+
+            if (!action(node))
+                return false;
+
+            InOrder(action, !reverse ? node.Right : node.Left, reverse);
+
+            return true;
+        }
+
+        internal virtual bool PreOrder(Predicate<Node<T>> action, Node<T> node)
+        {
+            if (node == null)
+                return false;
+
+
+            if (!action(node))
+                return false;
+
+            PreOrder(action, node);
+
+            PreOrder(action, node);
+
+            return true;
+
+        }
+
+        internal virtual bool PostOrder(Predicate<Node<T>> action, Node<T> node)
+        {
+            if (node == null)
+                return false;
+
+            PostOrder(action, node);
+
+            PostOrder(action, node);
+
+            if (!action(node))
+                return false;
+
+            return true;
+
+        }
+
+        public void Validate()
+        {
+
+        }
+
+
+        public void PathToLeafNode()
+        {
+
+
+        }
+
+
+
+        internal Node<T> FindNode(Node<T> rootNode, T item)
+        {
+            if (item is null || root is null) return null;
+
+            var current = rootNode;
+
+            var order = comparer.Compare(root.Item, item);
+
+            if (order == 0)
+                return current;
+            else if (order < 0)
+                return FindNode(current.Left, item);
+            else
+            {
+                return FindNode(current.Right, item);
+            }
+        }
+
+
+        internal Node<T> FindNode(Node<T> rootNode, T other, Func<Node<T>, T, bool> match)
+        {
+            if (rootNode is null) return null;
+
+            if (match == null) throw new ArgumentException("match");
+
+            var current = rootNode;
+
+            if (match(current, other)) return current;
+
+            var order = comparer.Compare(other, current.Item);
+
+            while (current != null)
+            {
+                if (order < 0)
+                    return FindNode(current.Left, other, match);
+
+                if (order > 0)
+                    return FindNode(current.Right, other, match);
+
+
+            }
+
+            return null;
+
+
+        }
+        public virtual bool Contains(T item)
+        {
+            return FindNode(root, item) != null;
         }
 
         public void CopyTo(T[] array, int arrayIndex)
@@ -136,7 +347,7 @@ namespace Binary_Search_Tree
 
         public void IntersectWith(IEnumerable<T> other)
         {
-            throw new NotImplementedException(); 
+            throw new NotImplementedException();
         }
 
         public bool IsProperSubsetOf(IEnumerable<T> other)
@@ -164,9 +375,65 @@ namespace Binary_Search_Tree
             throw new NotImplementedException();
         }
 
+
+        internal bool HasSiblings(Node<T> node) => node.Left is not null && node.Right is not null;
+        internal bool HasOneSiblings(Node<T> node) => (node.Left is not null && node.Right is null) || (node.Right is not null && node.Left is null);
+        internal bool IsLeafNode(Node<T> node) => (node.Left is null && node.Right is null);
+
+        /*
+       Algorithm :
+       Three Steps:
+
+       1. Find the node by traversing 
+           if node not found return null
+           if found check for below 3 cases 
+       2. if node has no children remove the node
+       3. if node has one child replace parent.Item with child.Item remove child
+       4. if node has two child
+              Replace data with inorder sucessor(Left most element of right subtree)
+
+           //You can also replace with inorder predecessor for removing node
+       */
+        internal Node<T> Remove(Node<T> node, T item)
+        {
+
+            if (node == null) return node;
+
+            var order = comparer.Compare(item, node.Item);
+
+            if (order < 0)
+                node.Left = Remove(node.Left, item);
+            else if (order > 0)
+                node.Right = Remove(node.Right, item);
+            else
+            {
+                if (IsLeafNode(node))
+                    node = null;
+                else if (HasSiblings(node))
+                {
+                    var current = node;
+                    while (current.Left != null)
+                        current = current.Left;
+
+                    var sucessor = current;
+
+                    node.Item = sucessor.Item;
+
+                    node.Right = Remove(node.Right, sucessor.Item);
+                }
+                else
+                {
+                    var child = node.Left != null ? node.Left : node.Right;
+                    node = child;
+                }
+            }
+
+            return node;
+
+        }
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            return Remove(root, item) != null;
         }
 
         public bool SetEquals(IEnumerable<T> other)
